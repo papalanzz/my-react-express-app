6e3e52b1c7e844d3833c8331804fe6935d6ceb53
Matt Lanzi <mattlanzi@me.com>
2020-08-13 16:05:31 -0400

    front end is crisp

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,9 +1,62 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { defaultState } from '../../server/defaultState';
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware();
+import * as sagas from './sagas.mock';
+import * as mutations from './mutations';
 
 export const store = createStore(
-  function reducer(state = defaultState, action){
-    return state;
-  }
+  combineReducers({
+    tasks(tasks = defaultState.tasks, action) {
+      switch (action.type) {
+        case mutations.CREATE_TASK:
+          console.log(action);
+          return [
+            ...tasks,
+            {
+              id: action.taskId,
+              name: 'New Task',
+              group: action.groupId,
+              owner: action.ownerId,
+              isComplete: false,
+            },
+          ];
+        case mutations.SET_TASK_COMPLETE:
+          return tasks.map((task) => {
+            return task.id === action.taskId
+              ? { ...task, isComplete: action.isComplete }
+              : task;
+          });
+        case mutations.SET_TASK_NAME:
+          return tasks.map((task) => {
+            return task.id === action.taskId
+              ? { ...task, name: action.name }
+              : task;
+          });
+        case mutations.SET_TASK_GROUP:
+          return tasks.map((task) => {
+            return task.id === action.taskId
+              ? { ...task, group: action.groupId }
+              : task;
+          });
+      }
+      return tasks;
+    },
+    comments(comments = defaultState.comments) {
+      return comments;
+    },
+    groups(groups = defaultState.groups) {
+      return groups;
+    },
+    users(users = defaultState.users) {
+      return users;
+    },
+  }),
+  applyMiddleware(createLogger(), sagaMiddleware)
+);
 
-)
+for (let saga in sagas) {
+  sagaMiddleware.run(sagas[saga]);
+}
---
+++ src/app/store/mutations.js
@@ -0,0 +1,35 @@
+export const REQUEST_TASK_CREATION = `REQUEST_TASK_CREATION`;
+export const CREATE_TASK = `CREATE_TASK`;
+export const SET_TASK_COMPLETE = 'SET_TASK_COMPLETE';
+export const SET_TASK_GROUP = 'SET_TASK_GROUP';
+export const SET_TASK_NAME = 'SET_TASK_NAME';
+
+export const requestTaskCreation = (groupId) => ({
+  type: REQUEST_TASK_CREATION,
+  groupId,
+});
+
+export const createTask = (taskId, groupId, ownerId) => ({
+  type: CREATE_TASK,
+  taskId,
+  groupId,
+  ownerId,
+});
+
+export const setTaskCompletion = (id, isComplete) => ({
+  type: SET_TASK_COMPLETE,
+  taskId: id,
+  isComplete
+});
+
+export const setTaskName = (id, name) => ({
+  type: SET_TASK_NAME,
+  taskId: id,
+  name
+});
+
+export const setTaskGroup = (id, groupId) => ({
+  type: SET_TASK_GROUP,
+  taskId: id,
+  groupId
+});
